16846b872eec145ba730accaf7ef4c8a5214d152
Antonio Forgianni <antonio.forgianni@mail.com>
2023-10-02 11:09:42 +0200

    assigned a name to the experimental

---
+++ cmm-dml-gen/output/output.sql
@@ -0,0 +1,119 @@
+INSERT INTO cmm.content (title, `text`, collection_id) VALUES(
+"1. TITLE ONE TITLE ONE",
+"blaone blaone blaone
+blaone blaone blaone
+blaone blaone blaone
+blaone blaone blaone
+blaone blaone \\"blaone\\"",
+1);
+
+INSERT INTO cmm.content (title, `text`, collection_id) VALUES(
+"2.  TITLE TWO TITLE TWO",
+"1. Im not a title 1
+blatwo blatwo blatwo
+blatwo blatwo blatwo
+2. Im not a title 2
+blatwo blatwo blatwo
+blatwo blatwo blatwo",
+1);
+
+INSERT INTO cmm.content (title, `text`, collection_id) VALUES(
+"281. IO TI ADORERO’ PER CHI TU SEI",
+"Stare qui e pensare a tutto il bene 
+Che mi hai fatto Tu.
+E restar fermo qui, la Tua dolce voce 
+ascoltar!
+Santo, Giusto, Fedele Sempre Sei
+Vero, Amico e mio Redentor 
+
+Coro:
+“Io Ti adorerò per Chi Tu Sei”X3 
+Gesù
+
+Al sicuro son, nel Tuo amor
+Che durerà per sempre  x2",
+1);
+
+INSERT INTO cmm.content (title, `text`, collection_id) VALUES(
+"9. TU PUOI NASCERE DI NUOVO",
+"1. Tu puoi nascere di nuovo X3
+E di nuovo cominciar 
+Non pensando più a ier 
+[Cominciando a camminar, 
+Cominciando a camminar 
+Con Gesù come pastor.] (bis)
+2. Tu puoi essere perdonato 
+Dall'intero tuo peccato 
+Si Gesù tutto pagò 
+[La Sua pace Egli portò 
+La Sua pace Egli portò 
+Puoi averla nel tuo cuor.] (bis)",
+1);
+
+INSERT INTO cmm.content (title, `text`, collection_id) VALUES(
+"138. NON TEMETE E NON VI SMARRITE",
+"Non temete e non vi smarrite,
+Non venga meno il vostro cuor.
+Del nemico non vi spaventate,
+Perché con voi marcia il Signor.
+Perciò combattiamo con fede e coraggio 
+Indossando la nostra armatura.
+Ovunque annunciamo il grande messaggio,
+Dell’amore di Dio in ogni terra….
+
+Coro:
+ Vittoria c’è nel figlio di Javhè     
+ Trionferemo, Chi resisterà, se Lui
+ combatterà per noi,
+ Per noi.
+ Vittoria c’è nel figlio di Javhè trionferemo,
+ Chi resisterà, se Lui combatterà per noi,
+ Noi vinceremo.",
+1);
+
+INSERT INTO cmm.content (title, `text`, collection_id) VALUES(
+"139. SALMO 18",
+"Signore io T’amo
+Tu la mia forza Sei
+L’Eterno è la mia rocca,
+Il mio liberatore!
+Il mio Dio Tu Sei 
+Mia rupe mio rifugio
+Mio scudo, mio ricetto, potente Salvator!
+
+
+Coro: 
+Benedetta la mia rocca, il Signore vive e regna
+Benedetto Sia l’Eterno, e perciò Lo loderò!
+
+Io invocai il Signore, ch’è degno d’ogni 
+lode
+Gridai al mio Dio, udì la mia voce
+Distese la Sua mano dall’alto mi afferrò
+Mi trasse fuori al largo ed Ei mi liberò!",
+1);
+
+INSERT INTO cmm.content (title, `text`, collection_id) VALUES(
+"139. SALMO 18",
+"SALMO 18
+
+Signore io T’amo
+Tu la mia forza Sei
+L’Eterno è la mia rocca,
+Il mio liberatore!
+Il mio Dio Tu Sei 
+Mia rupe mio rifugio
+Mio scudo, mio ricetto, potente Salvator!
+
+
+Coro: 
+Benedetta la mia rocca, il Signore vive e regna
+Benedetto Sia l’Eterno, e perciò Lo loderò!
+
+Io invocai il Signore, ch’è degno d’ogni 
+lode
+Gridai al mio Dio, udì la mia voce
+Distese la Sua mano dall’alto mi afferrò
+Mi trasse fuori al largo ed Ei mi liberò!",
+1);
+
