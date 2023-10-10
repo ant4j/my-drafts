e5fa39da9e30b7f54c547dbeb8f52c6ec03a1fdc
Antonio Forgianni <antonio.forgianni@mail.com>
2023-10-10 14:07:14 +0200

    corrections and formatting

--- cmm-dml-gen/cmm-dml-gen.pl
+++ cmm-dml-gen/cmm-dml-gen.pl
@@ -23,28 +23,34 @@ while(<FH>) {
     my $currentLine = "$_";
     chomp($currentLine);
 
-    if($currentLine =~ /\d{1,3}\.\ {0,3}([A-Z](\,|\Ì|\È|\À|\Ù|\Ò|\'|\.|\ |\’|\…|\.|\?|\(|\)|\d{1,3})*)+$/) { # first part of title
+    # first part of title
+    if($currentLine =~ /\d{1,3}\.\ {0,3}([A-Z](\,|\Ì|\È|\À|\Ù|\Ò|\'|\.|\ |\’|\…|\.|\?|\(|\)|\d{1,3})*)+$/) {
         
         composeDml();
 
-        $currentLine =~ s/^\s+|\s+$//g; # trim leading and trailing white spaces
+        # trim leading and trailing white spaces
+        $currentLine =~ s/^\s+|\s+$//g;
 
-        $currentLine =~ s/\'/\\'/g; # relace backslash with double backslash for single quote symbol
+        # replace backslash with double backslash for single quote symbol
+        $currentLine =~ s/\'/\\'/g;
 
         $title = $currentLine;
     
-    #                                           /([A-Z](\,|\Ì|\È|\À|\Ù|\Ò|\'|\.|\ |\’|\…|\.|\?|\(|\)|\d{1,3})*)+$/
-    } elsif($titlePartsCount < 2 and $currentLine =~ /([A-Z](\,|\Ì|\È|\À|\Ù|\Ò|\'|\.|\ |\’|\…|\.|\?|\(|\))*)+$/) { # possible second part of title
+    # possible second part of title
+    } elsif($titlePartsCount < 2 and $currentLine =~ /([A-Z](\,|\Ì|\È|\À|\Ù|\Ò|\'|\.|\ |\’|\…|\.|\?|\(|\))*)+$/) {
     
-        $currentLine =~ s/^\s+|\s+$//g; # trim leading and trailing white spaces
+        # trim leading and trailing white spaces
+        $currentLine =~ s/^\s+|\s+$//g;
 
-        $currentLine =~ s/\'/\\'/g; # relace backslash with double backslash for single quote symbol
+        # replace backslash with double backslash for single quote symbol
+        $currentLine =~ s/\'/\\'/g;
         
         $title = "$title"." $currentLine";
         
     } else { # text
 
-        $currentLine =~ s/\'/\\'/g; # relace backslash with double backslash for single quote symbol
+        # replace backslash with double backslash for single quote symbol
+        $currentLine =~ s/\'/\\'/g;
         
         $text = "$text"."$currentLine\n";
         
@@ -74,7 +80,8 @@ sub composeDml {
     
     if(length($title) > 0 and length($text) > 0) {
 
-        $text =~ s/^\s+|\s+$//g; # trim leading and trailing white spaces
+        # trim leading and trailing white spaces
+        $text =~ s/^\s+|\s+$//g;
 
         push(@items, "INSERT INTO cmm.content (title, `text`, collection_id) VALUES(\n'$title',\n'$text',\n$collectionId);");
         
